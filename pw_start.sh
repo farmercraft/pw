c982460aa7cc750bd72ae47df0ea853eff7cc29f
Zhi Wang <zhi.wang.linux@gmail.com>
2021-06-17 13:43:01 +0300

    pw_ftp: fix pw_start.sh
    Signed-off-by: Zhi Wang <zhi.wang.linux@gmail.com>

--- pw_start.sh
+++ pw_start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo `hostname` | sudo -S apt-get -y install python3-pip
+echo `hostname` | sudo -S apt-get -y install python3-pip wget lftp
 echo `hostname` | sudo -S pip3 install inotify psutil
-echo `hostname` | sudo -S pip3 pyftpdlib wget lftp
+echo `hostname` | sudo -S pip3 install pyftpdlib
 
 touch pw_conf.py
 ./pw_ftp.py &
