8c3e18999b93db61e41888647fd2732510dcf8e5
pisa <paizzj@126.com>
2022-09-30 19:10:26 +0800

    Update pw_install.sh

--- pw_install.sh
+++ pw_install.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/plotmanager/pw.git
 sudo cp -r pw /usr/local/pw
 sudo ln -s /usr/local/pw/pw_python.py  /usr/local/bin/pw
+sudo chmod a+rwx /usr/local/bin/pw
 echo `hostname` | sudo -S apt-get -y install python3-pip
 echo `hostname` | sudo -S pip3 install inotify psutil
 
