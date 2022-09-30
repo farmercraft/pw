c6f78cd1235f57f69588a3edfff828488f4d5c10
pisa <paizzj@126.com>
2022-09-30 18:25:37 +0800

    Update pw_install.sh

--- pw_install.sh
+++ pw_install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo cp -r pw /usr/local/pw
+sudo ln -s /usr/local/pw/pw_python.py  /usr/local/bin/pw
 echo `hostname` | sudo -S apt-get -y install python3-pip
 echo `hostname` | sudo -S pip3 install inotify psutil
 
