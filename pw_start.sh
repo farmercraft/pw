7088460d82792e2bc10e72c8a5ed45864c570328
z1 <paizzj@126.com>
2022-12-01 00:06:27 +0000

    start args

--- pw_start.sh
+++ pw_start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#echo `hostname` | sudo -S apt-get -y install python3-pip
-#echo `hostname` | sudo -S pip3 install inotify psutil
-
-./pw_python.py
+echo `hostname` | sudo -S apt-get -y install python3-pip
+echo `hostname` | sudo -S pip3 install inotify psutil
+echo `hostname` | sudo -S lsblk
+pw_python.py
